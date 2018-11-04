555c6c69d9bd53ddb388f1f718776b079d457a0e
ikram101 <syed.ikramali@gmail.com>
2018-11-04 15:41:28 -0500

    Renamed Project

---
+++ University/University/Data/UniversityContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using University.Models;
+
+namespace University.Data
+{
+    public class UniversityContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public UniversityContext()
+        {
+
+        }
+
+        
+
+    }
+}
