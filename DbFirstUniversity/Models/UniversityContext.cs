4fd957d850ef0fa8a1da16ef774c82fb0f04cb36
ikram101 <syed.ikramali@gmail.com>
2018-11-11 21:05:11 -0500

    Reverse engineered Univesity Database:
    Scaffold-DbContext "Server=(localdb)\mssqllocaldb;Database=University;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models

---
+++ DbFirstUniversity/Models/UniversityContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DbFirstUniversity.Models
+{
+    public partial class UniversityContext : DbContext
+    {
+        public UniversityContext()
+        {
+        }
+
+        public UniversityContext(DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Course { get; set; }
+        public virtual DbSet<Enrollment> Enrollment { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=University;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(e => e.CourseId)
+                    .HasColumnName("CourseID")
+                    .ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.HasIndex(e => e.CourseId);
+
+                entity.HasIndex(e => e.StudentId);
+
+                entity.Property(e => e.EnrollmentId).HasColumnName("EnrollmentID");
+
+                entity.Property(e => e.CourseId).HasColumnName("CourseID");
+
+                entity.Property(e => e.StudentId).HasColumnName("StudentID");
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.CourseId);
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.StudentId);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+            });
+        }
+    }
+}
