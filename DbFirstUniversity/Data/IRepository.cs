31c7773bc2035bc9cb37ca14290fc71132638b26
ikram101 <syed.ikramali@gmail.com>
2018-11-16 22:49:12 -0500

    staged files

---
+++ DbFirstUniversity/Data/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirstUniversity.Data
+{
+    public interface IRepository where TEntity : class
+    {
+        IEnumerable GetAll();
+        IEnumerable Find(Expression> predicate);
+        TEntity Get(object Id);
+        void Add(TEntity entity);
+        void AddRange(IEnumerable entities);
+        void Update(TEntity entity);
+        void Remove(object Id);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable entities);
+    }
+   
+
+}
