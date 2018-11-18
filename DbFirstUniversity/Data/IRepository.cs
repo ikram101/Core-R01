a338ac4a89f5b4c07759364b31fac68a9d30bfad
ikram101 <syed.ikramali@gmail.com>
2018-11-17 21:33:47 -0500

    Add generic Repository

--- DbFirstUniversity/Data/IRepository.cs
+++ DbFirstUniversity/Data/IRepository.cs
@@ -6,21 +6,61 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 using System.Data;
 using Microsoft.EntityFrameworkCore;
+using DbFirstUniversity.Models;
 
 namespace DbFirstUniversity.Data
 {
-    public interface IRepository where TEntity : class
+
+    public abstract class EntityBase
     {
-        IEnumerable GetAll();
-        IEnumerable Find(Expression> predicate);
-        TEntity Get(object Id);
-        void Add(TEntity entity);
-        void AddRange(IEnumerable entities);
-        void Update(TEntity entity);
-        void Remove(object Id);
-        void Remove(TEntity entity);
-        void RemoveRange(IEnumerable entities);
+        public int Id { get; protected set; }
     }
-   
+
+    public interface IRepository<T> where T : EntityBase
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+
+        protected readonly DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+           _context.Set<T>().Add(entity);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+             _context.Set<T>().Remove(entity);
+
+        }
+    }
+
+
 
 }
