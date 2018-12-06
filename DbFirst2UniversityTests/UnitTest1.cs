98be6043b9bba150964abcd7e761f3573690be9f
ikram101 <syed.ikramali@gmail.com>
2018-12-06 14:16:20 -0500

    Add Test Projects

---
+++ DbFirst2UniversityTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbFirst2UniversityTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
