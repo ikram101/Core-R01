1506840fdd123208972e69035c56060ec258b5f9
ikram101 <syed.ikramali@gmail.com>
2018-12-06 14:27:34 -0500

    Add refrences to test project

---
+++ DbFirstUniversity.Tests/HomeControllerIndexTests.cs
@@ -0,0 +1,27 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DbFirstUniversity.Controllers;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbFirstUniversity.Tests
+{
+    [TestClass]
+
+    public class HomeControllerIndexTests
+    {
+        [TestMethod]
+
+        public void HomeIndexTests()
+
+        {
+            HomeController homeController = new HomeController();
+
+            ViewResult result = homeController.Index() as ViewResult;
+
+            Assert.AreEqual("Hello",result.ViewData["Hello"]);
+
+        }
+
+    }
+}
