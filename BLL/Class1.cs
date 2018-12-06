ccf5028cf8a6cd7df7e2a9b2e1229900149853fc
ikram101 <syed.ikramali@gmail.com>
2018-12-06 14:48:15 -0500

    Add business Logic layer

---
+++ BLL/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BLL
+{
+    public class AccountDetails
+    {
+
+        public double m_balance { get; set; }
+
+        public void Withdraw(double amount)
+        {
+            m_balance = 100;
+
+            if (m_balance >= amount)
+            {
+                m_balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException(amount.ToString(), "Withdrawal exceeds balance!");
+            }
+        }
+    }
+}
